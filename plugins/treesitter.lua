7d193cca64f82fb6359397b01faf58cb043f40b1
Sankalp-G <sankalp.gawali@gmail.com>
2023-07-01 13:33:27 +0530

    enable autotags

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -32,6 +32,9 @@ return {
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
       },
+      autotag = {
+        enable = true,
+      },
     }
   end,
 }
